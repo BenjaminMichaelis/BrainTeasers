206128c0330943b837cf6bb5ef178ae239c61ee3
Austen Frostad <austen.frostad@intellitect.com>
2024-03-13 22:37:50 -0700

    add defaults test

---
+++ BrainTeasers.Tests/DefaultTests.cs
@@ -0,0 +1,29 @@
+using Xunit.Abstractions;
+
+namespace BrainTeasers.Tests;
+
+public class DefaultTests(ITestOutputHelper output)
+{
+    private readonly ITestOutputHelper _Output = output;
+
+    [Fact]
+    public void DefaultsQuiz()
+    {
+        // Assert
+        //Uncomment each and make them pass
+        //Assert.Equal(foo, default(int));
+        //Assert.Equal(foo, default(double));
+        //Assert.Equal(foo, default(float));
+        //Assert.Equal(foo, default(decimal));
+        //Assert.Equal(foo, default(long));
+        //Assert.Equal(foo, default(short));
+        //Assert.Equal(foo, default(char));
+        //Assert.Equal(foo, default(char?));
+        //Assert.Equal(foo, default(string?));
+        //Assert.Equal(foo, default(object));
+        //Assert.Equal(foo, default(DateTime));
+        //Assert.Equal(foo, default(DateTime?));
+        //Assert.Equal(foo, default(int?));
+
+    }
+}
