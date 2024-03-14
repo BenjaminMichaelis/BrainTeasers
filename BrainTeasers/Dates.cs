9cfd0bd1a960e7cba39d24175a06ed9945098587
Austen Frostad <austen.frostad@intellitect.com>
2024-03-13 22:04:22 -0700

    make compile

--- BrainTeasers/Dates.cs
+++ BrainTeasers/Dates.cs
@@ -7,16 +7,19 @@ public static class Dates
     public static bool IsLeapYear(DateTime date)
     {
         //TODO: return true if the date is in a leap year
+        return false;
     }
 
     public static bool IsDST(DateTime date)
     {
         //TODO: return true if the date is during Daylight Saving Time
         // How would you do this if the computer was in a different country?
+        return false;
     }
 
     public static DateTime TimeInTokyoJapan()
     {
         //TODO: return the current time in Tokyo, Japan
+        return DateTime.UtcNow;
     }
 }
